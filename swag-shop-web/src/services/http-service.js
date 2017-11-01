7c169e6ffe695e1b48b3becab8010a051fb39144
vanbumi <jsp.dio@gmaile.com>
2017-11-01 14:24:10 +0700

    error localhost 3001 swag-shop-web can not open

---
+++ swag-shop-web/src/services/http-service.js
@@ -0,0 +1,12 @@
+import 'whatwg-fetch';
+
+class HttpService {
+    getProducts = () {
+        fetch('http://localhost:3000/product')
+        .then(response => {
+            console.log(response.json());
+        })
+    }
+}
+
+export default HttpService;
